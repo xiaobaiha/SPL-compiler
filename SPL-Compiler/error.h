bf5ac1b975f45fa801e5de7801c78cd0c3b533a8
LucienXian <lucienxian97@gmail.com>
2018-06-01 14:19:06 +0800

    Update error.h

--- SPL-Compiler/error.h
+++ SPL-Compiler/error.h
@@ -1,6 +1,16 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+extern bool EM_anyErrors;
+
+void EM_newline(void);
+
+extern int EM_tokPos;
+
+void EM_error(int, string,...);
+void EM_impossible(string,...);
+void EM_reset(string filename);
+
 void fatal_error(const char *fmt, ... );
 
 #endif
